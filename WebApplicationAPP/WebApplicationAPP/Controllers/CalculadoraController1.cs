6ab7f8090e49aadc405766af6860a4421c83a217
JafethPM <poveda1390@gmail.com>
2026-01-25 18:27:08 -0600

    Calculadora

---
+++ WebApplicationAPP/WebApplicationAPP/Controllers/CalculadoraController1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CalculadoraViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculadoraViewModel modelo)
+        {
+
+            if (modelo.operacion == "suma")
+            {
+                modelo.resultado = modelo.valor1 + modelo.valor2;
+            }
+            else if (modelo.operacion == "resta")
+            {
+                modelo.resultado = modelo.valor1 - modelo.valor2;
+            }
+            else if (modelo.operacion == "multiplicacion")
+            {
+                modelo.resultado = modelo.valor1 * modelo.valor2;
+            }
+            else if (modelo.operacion == "division")
+            {
+                modelo.resultado = modelo.valor1 / modelo.valor2;
+            }
+
+            return View(modelo);
+        }
+    }
+}
